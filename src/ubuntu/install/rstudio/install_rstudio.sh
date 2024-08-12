887717e5d358f11a834bf1eeea705f85db85aa8a
Albrecht Gebhardt <alge@algepop.net>
2024-08-12 13:09:52 +0200

    add R and quarto

--- src/ubuntu/install/rstudio/install_rstudio.sh
+++ src/ubuntu/install/rstudio/install_rstudio.sh
@@ -5,6 +5,15 @@ set -ex
 wget -q https://download1.rstudio.org/electron/jammy/amd64/rstudio-2024.04.2-764-amd64.deb -O rstudio-2024.04.2-764-amd64.deb
 apt-get update
 apt-get install -y ./rstudio-2024.04.2-764-amd64.deb
+# install quarto
+wget -q https://github.com/quarto-dev/quarto-cli/releases/download/v1.5.56/quarto-1.5.56-linux-amd64.deb -O quarto-1.5.56-linux-amd64.deb
+apt-get install -y ./quarto-1.5.56-linux-amd64.deb
+# install R
+apt-get install --no-install-recommends software-properties-common dirmngr
+wget -qO- https://cloud.r-project.org/bin/linux/ubuntu/marutter_pubkey.asc | sudo tee -a /etc/apt/trusted.gpg.d/cran_ubuntu_key.asc
+add-apt-repository "deb https://cloud.r-project.org/bin/linux/ubuntu $(lsb_release -cs)-cran40/"
+apt-get install -y --install-recommends r-base-dev
+
 
 # Cleanup for app layer
 chown -R 1000:0 $HOME
