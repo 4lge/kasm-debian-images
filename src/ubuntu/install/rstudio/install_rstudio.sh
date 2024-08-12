e096b865422fe16e49804fd96560b95d86eab11f
Albrecht Gebhardt <alge@algepop.net>
2024-08-12 11:33:36 +0200

    initialize from a subset of workspaces-images-develop add rstudio

---
+++ src/ubuntu/install/rstudio/install_rstudio.sh
@@ -0,0 +1,25 @@
+#!/usr/bin/env bash
+set -ex
+
+# Install RStudio
+ARCH=$(arch | sed 's/aarch64/arm64/g' | sed 's/x86_64/x64/g')
+if [ "${ARCH}" -ne "amd64" ]; then
+  echo "only amd64 in debian/ubuntu supported"
+  exit 1
+else
+wget -q https://https://download1.rstudio.org/electron/jammy/amd64/rstudio-2024.04.2-764-${ARCH}.deb -O rstudio-2024.04.2-764-${ARCH}.deb
+apt-get update
+apt-get install -y ./rstudio-2024.04.2-764-${ARCH}.deb
+
+# Cleanup for app layer
+chown -R 1000:0 $HOME
+find /usr/share/ -name "icon-theme.cache" -exec rm -f {} \;
+if [ -z ${SKIP_CLEAN+x} ]; then
+  apt-get autoclean
+  rm -rf \
+    /var/lib/apt/lists/* \
+    /var/tmp/* \
+    /tmp/*
+fi
+
+fi # not amd64
